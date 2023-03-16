1567d4395fb21d24ee90acea0062656195b601db
a-r-a-d-e-m-m <affiammedara@outlook.com>
2023-03-16 16:58:59 -0700

    C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+/**
+ *main - prints the size of data types.
+ *
+ *Return - a 0 means all is good.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+return (0);
+}
