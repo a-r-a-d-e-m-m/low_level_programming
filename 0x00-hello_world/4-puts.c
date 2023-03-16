daccd8124a186fffdb3db8dd604fa6314ac1eab6
a-r-a-d-e-m-m <affiammedara@outlook.com>
2023-03-16 15:40:35 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 /**
-*This prints the text:
-*"Programming is like building a multilingual puzzle.
+*This prints the text: "Programming is like building a multilingual puzzle.
 *
 *Description: Printing with an alternative to printf
 */
 
 int main(void)
 {
-	char a[]= "\"Programming is like building a multilingual puzzle \n";
-	
+	char a[] = "\"Programming is like building a multilingual puzzle \n";	
+
 	puts(a);
-	
+/*Description: Returns a value of zero and progran's good*/
 	return (0);
 }
