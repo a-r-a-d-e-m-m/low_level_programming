04fab0c7438053c4906da96364dc732872967ef6
a-r-a-d-e-m-m <affiammedara@outlook.com>
2023-03-16 21:19:33 -0700

    C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
 /**
  *main - prints text to the standard error.
  *
- *Return: returns.
+ *Return: 1 is succesful.
  */
 
 #include <stdio.h>
-
+#include <unistd.h>
 int main(void)
 
 {
-fwrite("2, \"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+fputs("\and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 
 return (1);
 }
