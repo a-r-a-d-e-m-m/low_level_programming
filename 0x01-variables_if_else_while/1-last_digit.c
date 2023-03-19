0d143ba36351c716cc03117114cc26c06b697255
a-r-a-d-e-m-m <affiammedara@outlook.com>
2023-03-19 09:46:19 -0700

    C: Variables and Loops

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,10 +12,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-int d = n % 10;
+int n = n % 10;
 
 if (n > 5)
-printf("Last digit of %d is %d greater than 5\n", n, d);
+printf("Last digit of %d is %d greater than 5\n", n, n);
 
 if (n == 0)
 printf("Last digit of %d is 0\n", n);
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - prints the alphabet in lower case.
+*Return: while loop.
+*/
+
+int main(void)
+{
+
+int putchar (int c);
+
+for (int c = 'a'; c <= 'z'; c++)
+
+putchar('\n');
+
+return (0);
+}
