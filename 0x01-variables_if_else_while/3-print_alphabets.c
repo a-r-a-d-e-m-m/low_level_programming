277474e579806aead395715da1571ce079fb03e4
a-r-a-d-e-m-m <affiammedara@outlook.com>
2023-03-21 03:28:23 -0700

    C: Variables and Loops

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,18 @@
-ill beack 
+#include <stdio.h>
+/**
+*main - prints the alphabet in lower and then upper case.
+*Return: A success.
+*/
+
+int main(void)
+{
+char lc;
+
+for (lc = 'a'; lc <= 'z'; lc++)
+putchar(lc);
+
+for (lc = 'A'; lc <= 'Z'; lc++)
+putchar(lc);
+putchar('\n');
+return (0);
+}
