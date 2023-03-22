6b873bac1f203ed2596e8e7ec28ee85af09d5b79
a-r-a-d-e-m-m <affiammedara@outlook.com>
2023-03-22 04:22:39 -0700

    C's functions and nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
-
-	/**
-	 * void times_table - prints the 9 times table, starting with 0
-	 */
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
 	void times_table(void)
 	{
 		int i, j, k;
