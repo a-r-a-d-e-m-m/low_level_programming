d2c870c8d8238c7f9f57d1db65587f590baf423a
a-r-a-d-e-m-m <affiammedara@outlook.com>
2023-03-25 08:24:06 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
+#include <stdio.h>
 /**
 *main - This source prints with an alternative to printf.
 *
 *Return: When 0, code is fine.
 */
 
-#include <stdio.h>
-
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
