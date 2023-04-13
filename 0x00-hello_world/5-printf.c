b19975e03d9e0ea885849932093e24e6e9e1c8f8
a-r-a-d-e-m-m <affiammedara@outlook.com>
2023-04-13 05:57:24 -0700

    C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
+#include <stdio.h>
+
 /**
  *main - makes the progam print text
  *
  *Return: zero proves that there are no bugs
  */
-#include <stdio.h>
 
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,\n");
+{
+printf("with proper grammar, but the outcome is a piece of art, \n");
+
 return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,10 @@
-/**
- *main - prints the size of data types.
- *
- *Return: a 0 means all is good.
- */
 #include <stdio.h>
+
+/**
+ *main - prints the size of data types on the computer it is compiled on.
+ *
+ *Return: always 0 means all is good.
+ */
 
 int main(void)
 {
@@ -13,10 +14,10 @@ long long int c;
 char d;
 float f;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", sizeof(d));
+printf("Size of an int: %lu byte(s)\n", sizeof(a));
+printf("Size of a long int: %lu byte(s)\n", sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
+printf("Size of a float: %lu byte(s)\n", sizeof(f));
 return (0);
 }
