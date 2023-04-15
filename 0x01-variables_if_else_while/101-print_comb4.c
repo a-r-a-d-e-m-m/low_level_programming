27c8f40582a53e308709aef2e41155c4f6e93375
a-r-a-d-e-m-m <affiammedara@outlook.com>
2023-04-14 23:44:23 -0700

    C: Variables and Loops

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,32 +2,34 @@
 
 /**
  * main - prints all possible different combinations of three digits
- * Return: Always 0 (Success)
+ * Return: A zero.
  */
+
 int main(void)
 {
-	int n, m, l;
 
-	for (n = 48; n < 58; n++)
-	{
-		for (m = 49; m < 58; m++)
-		{
-			for (l = 50; l < 58; l++)
-			{
-				if (l > m && m > n)
-				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
-}
+int a, b, c;
+
+for (a = 48; a < 58; a++)
+{
+
+for (b = 49; b < 58; b++)
+{
+
+for (c = 50; c < 58; c++)
+{
+
+if (c > b && b > a)
+{
+putchar(a);
+putchar(b);
+putchar(c);
+
+if (a != 55 || b != 56)
+{
+putchar(',');
+putchar(' '); } } } } }
+
+putchar('\n');
+
+return (0); }
