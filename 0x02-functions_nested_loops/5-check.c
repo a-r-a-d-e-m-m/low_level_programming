9711f48505fecc836dfdea920f2685ddd6f320fa
a-r-a-d-e-m-m <affiammedara@outlook.com>
2023-04-18 14:28:34 -0700

    C's functions and nested loops

--- 0x02-functions_nested_loops/5-check.c
+++ 0x02-functions_nested_loops/5-check.c
@@ -14,7 +14,7 @@ int print_sign(int i)
 if (i > 0)
 
 {
-_putchar(43);
+_putchar('+');
 return (1);
 }
 
@@ -28,7 +28,7 @@ return (0);
 else if (i < 0)
 
 {
-_putchar(45);
+_putchar('-');
 }
 
 return (-1);
